51317e69355bae3de32003cef0fdcf478444daf5
niavora <randrianekenaniavo@gmail.com>
2023-02-08 14:09:19 +0100

    footer

--- application/views/page/footer.php
+++ application/views/page/footer.php
@@ -1,6 +1,7 @@
 <footer class="py-5 bg-dark">
     <div class="container">
       <p class="m-0 text-center text-white">MATHIEU ETU2166 - NANDRAINA ETU2199 - ANTSA ETU2193</p>
+      <p>projet takalo</p>
     </div>
     <!-- /.container -->
   </footer>
